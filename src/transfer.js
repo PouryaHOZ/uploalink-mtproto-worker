34ad197c34a9579ddfc89ead1f4b95d6310b6193
PouryaHOZ <puryah1385@gmail.com>
2026-08-04 01:16:52 +0330

    fix(transfer): repair unclosed string literal in data split
    - fix(transfer): close string literal in data.toString().split('\n')

--- src/transfer.js
+++ src/transfer.js
@@ -255,6 +255,7 @@ class FileTransferBot {
             this.activeFFmpegProcess.stdout.on('data', data => {
                 const lines = data.toString().split('
 ');
+');
                 for (const line of lines) {
                     const [key, val] = line.split('=').map(s => s ? s.trim() : '');
                     if (key === 'out_time') {
