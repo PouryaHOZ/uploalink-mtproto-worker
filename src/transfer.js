090a29441ecfdab9a88f91e65b207e7afe468051
PouryaHOZ <puryah1385@gmail.com>
2026-08-04 01:36:36 +0330

    fix(workflow): install ffmpeg dependency on runner prior to transfer execution (v0.5.2)

--- src/transfer.js
+++ src/transfer.js
@@ -83,7 +83,7 @@ function drawProgressBar(percent, length = 10) {
     return "█".repeat(filled) + "░".repeat(length - filled);
 }
 
-const SYSTEM_VERSION = '0.5.3';
+const SYSTEM_VERSION = '0.5.2';
 
 function renderProgressCard({ fileName, masterPercent, stageName, stagePercent, speedText, etaText, detailsText }) {
     const masterBar = drawProgressBar(masterPercent, 12);
